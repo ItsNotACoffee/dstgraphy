687048ff4bf998b24a43261cf5f3b7b2cf47fcce
ItsNotACoffee <kristian.naglic@gmail.com>
2021-01-14 12:02:51 +0100

    - Added Merge, commit, branch functions - Added a graph view to represent the actions - Made nodes and edges colored according to the branch commited on - Made the design dark for all elements

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React from "react";
 import GraphMenu from './components/Graphs/GraphMenu';
 import Graph from "react-graph-vis";
+import update from 'immutability-helper';
+import { v4 as uuidv4 } from "uuid";
 import './App.css';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
 
-    this.state={
-      branches: []
-    };
-
-    this.graph = {
-      nodes: [
-        { id: 1, label: "Node 1", title: "node 1 tootip text" },
-        { id: 2, label: "Node 2", title: "node 2 tootip text" },
-        { id: 3, label: "Node 3", title: "node 3 tootip text" },
-        { id: 4, label: "Node 4", title: "node 4 tootip text" },
-        { id: 5, label: "Node 5", title: "node 5 tootip text" }
-      ],
-      edges: [
-        { from: 1, to: 2 },
-        { from: 1, to: 3 },
-        { from: 2, to: 4 },
-        { from: 2, to: 5 },
-        { from: 3, to: 5}
-      ]
+    this.state = {
+      branches: [{
+        name: "master",
+        nodes: [
+          1
+        ],
+        colorIndex: 7,
+        lastNode: 1, //keeps track of the last node added
+        initialNode: 1, //the starting point of the branch, can be a fork
+        finalNode: 0 //the final node of the branch, used when merging
+      }],
+      graphData: {
+        nodes: [
+          { id: 1, label: "Initial Commit", title: "Initial Commit" }
+        ],
+        edges: [] //{ from: 1, to: 2 }
+      },
+      nodeId: 1, //global counter for node ids
+      colors: {
+        index: 0,
+        cList: ["#e53935", "#43a047", "#fdd835", "#fb8c00", "#6d4c41", "#5e35b1", "#d81b60", "#63ccff"]
+      }
     };
 
     this.options = {
@@ -37,17 +43,26 @@ class App extends React.Component {
         }
       },
       nodes: {
+        shape: "hexagon",
         color: {
-          border: "#ffffff",
+          border: "#63ccff",
+          background: "#63ccff",
           highlight: {
             border: "#ffffff"
           }
         },
         fixed: true,
-        physics: false
+        physics: false,
+        font: {
+          color: "#ffffff"
+        }
       },
       edges: {
-        color: "#ffffff"
+        color: "#ffffff",
+        width: 3
+      },
+      interaction: {
+        hover: true
       },
       height: "500px"
     };
@@ -56,22 +71,89 @@ class App extends React.Component {
     this.merge = this.merge.bind(this);
     this.addBranch = this.addBranch.bind(this);
     this.removeBranch = this.removeBranch.bind(this);
+    this.print = this.print.bind(this);
   }
 
-  commit(branch, msg) {
+  commit(branchName, msg) {
+    //update the nodeId tracker
+    var currId = this.state.nodeId;
+    var updatedNodeId = update(this.state.nodeId, {$set: currId + 1});
+    this.setState({nodeId: updatedNodeId});
 
+    //update branch properties
+    var branch = this.state.branches.find(element => element.name === branchName);
+    // eslint-disable-next-line
+    var index = this.state.branches.findIndex(element => element.name === branchName);
+    
+    branch.nodes.push(updatedNodeId);
+    var lastNode = branch.lastNode;
+    branch.lastNode = updatedNodeId;
+    var updatedBranches = update(this.state.branches, {index: {$set: branch}});
+    this.setState({branches: updatedBranches});
+
+    //update graph properties
+    var color = this.state.colors.cList[branch.colorIndex];
+    var updatedData = update(this.state.graphData, {nodes: {$push: [{id: updatedNodeId, label: branch.name + " - " + msg, title: msg, color: color}]}});
+    updatedData = update(updatedData, {edges: {$push: [{from: lastNode, to: updatedNodeId, color: color}]}});
+    this.setState({graphData: updatedData});
   }
 
   merge(sBranch, dBranch) {
+    //--make a merge commit first--
+    //update the nodeId tracker
+    var currId = this.state.nodeId;
+    var updatedNodeId = update(this.state.nodeId, {$set: currId + 1});
+    this.setState({nodeId: updatedNodeId});
 
+    //fetch branches
+    var destBranch = this.state.branches.find(element => element.name === dBranch);
+    var sourceBranch = this.state.branches.find(element => element.name === sBranch);
+    // eslint-disable-next-line
+    var dIndex = this.state.branches.findIndex(element => element.name === dBranch);
+    // eslint-disable-next-line
+    var sIndex = this.state.branches.findIndex(element => element.name === sBranch);
+
+    //update branch properties
+    destBranch.nodes.push(updatedNodeId);
+    var lastNode = destBranch.lastNode;
+    destBranch.lastNode = updatedNodeId;
+    sourceBranch.finalNode = updatedNodeId;
+    var updatedBranches = update(this.state.branches, {dIndex: {$set: destBranch}});
+    updatedBranches = update(updatedBranches, {sIndex: {$set: sourceBranch}});
+    this.setState({branches: updatedBranches});
+
+    //update graph properties
+    var color = this.state.colors.cList[destBranch.colorIndex];
+    var updatedData = update(this.state.graphData, {nodes: {$push: [{id: updatedNodeId, label: "Merge " + sBranch + " into " + dBranch, title: "Merge " + sBranch + " into " + dBranch, color: color}]}});
+    updatedData = update(updatedData, {edges: {$push: [{from: lastNode, to: updatedNodeId, color: color}]}});
+    color = this.state.colors.cList[sourceBranch.colorIndex];
+    updatedData = update(updatedData, {edges: {$push: [{from: sourceBranch.lastNode, to: updatedNodeId, color: color}]}});
+    this.setState({graphData: updatedData});
   }
 
-  addBranch(bName) {
+  addBranch(bName, bCurrent) {
+    var branch = this.state.branches.find(element => element.name === bCurrent);
 
+    var colors = this.state.colors;
+    var updatedBranches = update(this.state.branches, {$push: [{name: bName, nodes: [], colorIndex: colors.index, lastNode: branch.lastNode, initialNode: 0, finalNode: 0}]});
+    this.setState({branches: updatedBranches});
+
+    //update color index
+    if (colors.index + 1 > 7) {
+      colors.index = 0;
+    } else {
+      colors.index = colors.index + 1;
+    }
+    var updatedColors = update(this.state.colors, {$set: colors});
+    this.setState({colors: updatedColors});
   }
 
   removeBranch(bName) {
 
+  }
+
+  print() {
+    console.log(this.state);
   }
 
   render() {
@@ -80,10 +162,10 @@ class App extends React.Component {
         <h1 className="title">DSTGraphy</h1>
         <h4 className="desc">~Simulate and visualize Github operations~</h4>
         <div>
-          <GraphMenu onBranch={this.addBranch} onCommit={this.commit}/>
+          <GraphMenu onBranch={this.addBranch} onCommit={this.commit} onMerge={this.merge} onPrint={this.print}/>
         </div>
         <div className="mainGraph">
-        <Graph graph={this.graph} options={this.options}/>
+        <Graph key={uuidv4} graph={this.state.graphData} options={this.options}/>
         </div>
       </div>
     );
--- src/components/Graphs/GraphMenu.js
+++ src/components/Graphs/GraphMenu.js
@@ -1,4 +1,5 @@
 import React from  "react";
+import update from 'immutability-helper';
 import "./GraphMenu.css";
 
 class GraphMenu extends React.Component {
@@ -7,39 +8,104 @@ class GraphMenu extends React.Component {
 
         this.state = {
             commitName: "",
-            branchName: ""
+            branchName: "",
+            selectedBranch: "master",
+            selectedMerge: "",
+            branchList: [
+                { value: "master", name: "master"}
+            ],
+            allBranchList: [ //used for tracking every branch added
+                { value: "master", name: "master"}
+            ]
         }
 
         this.setCommitName = this.setCommitName.bind(this);
         this.setBranchName = this.setBranchName.bind(this);
+        this.setSelectedBranch = this.setSelectedBranch.bind(this);
+        this.setSelectedMerge = this.setSelectedMerge.bind(this);
+
         this.passCommit = this.passCommit.bind(this);
         this.passBranch = this.passBranch.bind(this);
+        this.passMerge = this.passMerge.bind(this);
+        this.passPrint = this.passPrint.bind(this);
     }
 
     setCommitName(event) {
         this.setState({commitName: event.target.value});
     }
     setBranchName(event) {
-        this.setState({branchname: event.target.value});
+        this.setState({branchName: event.target.value});
+    }
+    setSelectedBranch(event) {
+        this.setState({selectedBranch: event.target.value});
+    }
+    setSelectedMerge(event) {
+        this.setState({selectedMerge: event.target.value});
     }
 
     passCommit() {
-        this.props.onCommit(this.state.commitName);
+        this.props.onCommit(this.state.selectedBranch, this.state.commitName);
     }
     passBranch() {
-        this.props.onBranch(this.state.branchName);
+        if (this.state.branchName !== "") {
+            var index = this.state.allBranchList.findIndex(element => element.value === this.state.branchName);
+            if (index === -1) {
+                this.setState({selectedMerge: this.state.branchList[0].value});
+
+                var updatedBranchList = update(this.state.branchList, {$push: [{ value: this.state.branchName, name: this.state.branchName}]});
+                this.setState({branchList: updatedBranchList});
+                updatedBranchList = update(this.state.allBranchList, {$push: [{ value: this.state.branchName, name: this.state.branchName}]});
+                this.setState({allBranchList: updatedBranchList});
+                this.props.onBranch(this.state.branchName, this.state.selectedBranch);
+            }
+        }
+    }
+    passMerge() {
+        if (this.state.selectedMerge !== "" && this.state.selectedMerge !== this.state.selectedBranch) {
+            this.props.onMerge(this.state.selectedBranch, this.state.selectedMerge);
+
+            var index = this.state.branchList.findIndex(element => element.value === this.state.selectedBranch);
+            var updatedBranchList = update(this.state.branchList, {$splice: [[index, 1]]});
+            this.setState({branchList: updatedBranchList});
+            this.setState({selectedBranch: this.state.selectedMerge});
+        }
+    }
+    passPrint() {
+        this.props.onPrint();
     }
 
     render() {
         return (
             <div className="graphMenu">
                 <div>
-                    <input placeholder="Commit name..." onChange={this.setCommitName}/>
-                    <button onClick={this.passCommit}>Commit</button>
+                    <b>Current Branch: </b>
+                    <select onChange={this.setSelectedBranch}>
+                        {this.state.branchList.map((e, key) => {
+                            return <option key={e.value} value={e.value}>{e.name}</option>;
+                        })}
+                    </select>
                 </div>
                 <div>
-                    <input placeholder="Branch name..." onChange={this.setBranchName}/>
-                    <button onClick={this.passBranch}>Branch</button>
+                    <input className="input" placeholder="Branch name..." onChange={this.setBranchName}/>
+                    <button className="button" onClick={this.passBranch}>Branch</button>
+                </div>
+                <div>
+                    <input className="input" placeholder="Commit name..." onChange={this.setCommitName}/>
+                    <button className="button" onClick={this.passCommit}>Commit</button>
+                </div>
+                <div>
+                    <select onChange={this.setSelectedMerge}>
+                        {this.state.branchList.map((e, key) => {
+                            if (this.state.branchList.length < 2) {
+                                return ""
+                            }
+                            else return <option key={e.value} value={e.value}>{e.name}</option>;
+                        })}
+                    </select>
+                    <button className="button" onClick={this.passMerge}>Merge</button>
+                </div>
+                <div>
+                    <button className="button" onClick={this.passPrint}>Print graph console</button>
                 </div>
             </div>
         );
